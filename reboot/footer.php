a0606030b0641e6d9769d199189f009fc75a9d2c
Matthew Isaac Lean <matt@mattlean.com>
2016-04-30 02:17:52 -0700

    Import Bootstrap carousel example

---
+++ reboot/footer.php
@@ -0,0 +1,11 @@
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo get_template_directory_uri(); ?>/node_modules/jquery/dist/jquery.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="<?php echo get_template_directory_uri(); ?>/js/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="<?php echo get_template_directory_uri(); ?>/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
