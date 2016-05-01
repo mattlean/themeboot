da459e76c2ea1977874524612c2c47b6b6f2aee6
Matthew Isaac Lean <matt@mattlean.com>
2016-05-01 15:17:46 -0700

    Setup blog feed

--- reboot/index.php
+++ reboot/index.php
@@ -1,4 +1,16 @@
 <?php get_header(); ?>
+    <div class="banner"></div>
+
     <div class="container marketing">
-        <div class="banner"></div>
+        <?php while ( have_posts() ) : the_post() ?>
+            <div id="post-<?php the_ID(); ?>" <?php post_class('post-container'); ?>" >
+                <h1><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_s' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+                <?php if ( 'post' == get_post_type() ) : ?>
+                <div class="post-meta-container text-muted">
+                    Posted by <strong><?php echo the_author_link(); ?></strong> on <strong><?php echo get_the_date(); ?></strong></a>
+                </div>
+                <?php endif; ?>
+                <?php the_content(); ?>
+            </div>
+        <?php endwhile; ?>
 <?php get_footer(); ?>
