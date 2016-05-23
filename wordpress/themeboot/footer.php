e6f47cbae36cb576c97b08504525a056e4848165
Matthew Isaac Lean <matt@mattlean.com>
2016-05-22 20:12:01 -0700

    Reformat repo, version original Bootstrap carousel example

--- wordpress/themeboot/footer.php
+++ wordpress/themeboot/footer.php
@@ -1,9 +1,26 @@
-		<footer>
-			<p class="pull-right"><a href="https://facebook.com/groups/santacruzfgc"><img src="/img/fb.png" alt="Santa Cruz FGC Facebook Group"></a></p>
-			<p>&copy; 2014-2015 Santa Cruz Fighting Game Community &middot; <a href="/under-construction.html">Constitution</a> &middot; <a href="/under-construction.html">Sponsors</a></p>
-		</footer>
-	</div>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.11.2.min.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
-</body>
+      <hr class="featurette-divider">
+
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>
+            &copy; 2014-2016 <?php bloginfo('name'); ?>
+            <?php wp_nav_menu(array('container' => false, 'menu_class' => 'list-inline', 'theme_location' => 'footer-nav-menu') ); ?>
+        </p>
+      </footer>
+
+    </div><!-- /.container -->
+
+    <script src="<?php echo get_template_directory_uri(); ?>/node_modules/jquery/dist/jquery.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/holder.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/ie10-viewport-bug-workaround.js"></script>
+    <?php
+      $cscript_vals = get_post_custom_values('cscript');
+
+      if($cscript_vals) {
+          generate_cscripts($cscript_vals);
+      }
+    ?>
+    <?php wp_footer(); ?>
+  </body>
 </html>
